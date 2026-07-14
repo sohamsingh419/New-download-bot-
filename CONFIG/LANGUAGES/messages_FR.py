6e010689c2aee5618f3889e5570f9bf3b66d8e74
sohamsingh419 <skhackeriam@gmail.com>
2026-07-14 19:33:41 +0530

    Update messages_FR.py

--- CONFIG/LANGUAGES/messages_FR.py
+++ CONFIG/LANGUAGES/messages_FR.py
@@ -173,8 +173,8 @@ Utilisez <code>/img URL</code> pour télécharger des images/photos/albums depui
         "</pre>\n"
         "<blockquote>"
         "<b><u>Instructions :</u></b>\n"
-        "https://t.me/tg_ytdlp/203 \n"
-        "https://t.me/tg_ytdlp/214 "
+        "https://t.me/Globall_X \n"
+        "https://t.me/Globall_X "
         "</blockquote>"
     )
     
