3b188c60554785714fa0c89d4632029fef43e81e
sohamsingh419 <skhackeriam@gmail.com>
2026-07-14 20:11:27 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- CONFIG/LANGUAGES/messages_PL.py
+++ CONFIG/LANGUAGES/messages_PL.py
@@ -173,8 +173,8 @@ Użyj <code>/img URL</code>, aby pobrać obrazy/zdjęcia/albumy z wielu platform
         "</pre>\n"
         "<blockquote>"
         "<b><u>Instrukcje:</u></b>\n"
-        "https://t.me/tg_ytdlp/203 \n"
-        "https://t.me/tg_ytdlp/214 "
+        "https://t.me/Globall_X \n"
+        "https://t.me/Globall_X "
         "</blockquote>"
     )
     
