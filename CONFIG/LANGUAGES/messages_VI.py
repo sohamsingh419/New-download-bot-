b410a994bd38dfa689f6fde6401d0a726efcedb0
sohamsingh419 <skhackeriam@gmail.com>
2026-07-14 20:53:12 +0530

    Update messages_VI.py

--- CONFIG/LANGUAGES/messages_VI.py
+++ CONFIG/LANGUAGES/messages_VI.py
@@ -173,8 +173,8 @@ Sử dụng <code>/img URL</code> để tải xuống hình ảnh/ảnh/album t�
         "</pre>\n"
         "<blockquote>"
         "<b><u>Instructions:</u></b>\n"
-        "https://t.me/tg_ytdlp/203 \n"
-        "https://t.me/tg_ytdlp/214 "
+        "https://t.me/Globall_X \n"
+        "https://t.me/Globall_X "
         "</blockquote>"
     )
     
